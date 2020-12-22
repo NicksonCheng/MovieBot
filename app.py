c0ab03e08dc5fb829c8112e0f9fed7ad2a769e91
nihao-hello <zz20102aa@gmail.com>
2020-12-23 01:00:18 +0800

    change to localhost

--- app.py
+++ app.py
@@ -117,4 +117,6 @@ def show_fsm():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="localhost", port=port, debug=True)
+
+# test
